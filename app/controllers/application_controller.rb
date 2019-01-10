47843cf9627b04dfcc675563002172abccc1a94f
Sharmaine Requerme <srqrme@gmail.com>
2019-01-10 01:38:54 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,15 +41,15 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
 
-  patch '/articles/:id' do
+  patch "/articles/:id" do
     @article = Article.find(params[:id])
     @article.update(params[:article])
-    redirect to '/articles/#{@article.id}'
+    redirect to "/articles/#{@article.id}"
   end
 
-  delete '/articles/:id' do
+  delete "/articles/:id" do
     Article.destroy(params[:id])
-    redirect to '/articles'
+    redirect to "/articles"
   end
 
 end
