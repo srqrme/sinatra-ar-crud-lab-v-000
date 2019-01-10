5e2c149df46369513932670a2e58920907107a0d
Sharmaine Requerme <srqrme@gmail.com>
2019-01-10 01:36:27 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < Sinatra::Base
     erb :new
   end
 
-  post '/articles' do
+  post "/articles" do
     @article = Article.create(params)
 
     redirect to '/articles/#{@article.id}'
